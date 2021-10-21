80de2aed6fa26c5c6c0639e0dbf077bd088035cc
Shreelatha <shreelathan@gmail.com>
2021-10-21 16:04:12 +0530

    updated version in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = "shreelatha"
 
 setuptools.setup(
     name=f"{PROJECT_NAME} - {USER_NAME}",
-    version="0.0.2",
+    version="0.0.3",
     author=USER_NAME,
     author_email="shreelathan@gmail.com",
     description="It's an implementation of perceptron",
